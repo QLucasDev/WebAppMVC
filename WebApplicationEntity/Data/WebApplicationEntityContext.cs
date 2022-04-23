55102107132994661e133574823187dc78a981cb
Lucas Quintão <qlucas.dev@gmail.com>
2022-04-23 12:30:06 -0300

    CRUD Scaffolding.

---
+++ WebApplicationEntity/Data/WebApplicationEntityContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationEntity.Models;
+
+namespace WebApplicationEntity.Data
+{
+    public class WebApplicationEntityContext : DbContext
+    {
+        public WebApplicationEntityContext (DbContextOptions<WebApplicationEntityContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplicationEntity.Models.Department> Department { get; set; }
+    }
+}
--- WebApplicationEntity/Startup.cs
+++ WebApplicationEntity/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationEntity.Data;
 
 namespace WebApplicationEntity
 {
@@ -33,6 +35,9 @@ namespace WebApplicationEntity
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebApplicationEntityContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebApplicationEntityContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
