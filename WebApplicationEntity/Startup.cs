98fce4eeea218c25e09e0d08283f1c9224200635
Lucas Quintão <qlucas.dev@gmail.com>
2022-04-24 20:25:55 -0300

    MySql adaptation and first migration

--- WebApplicationEntity/Startup.cs
+++ WebApplicationEntity/Startup.cs
@@ -37,7 +37,8 @@ namespace WebApplicationEntity
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebApplicationEntityContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebApplicationEntityContext")));
+                    options.UseMySql(Configuration.GetConnectionString("WebApplicationEntityContext"), builder =>
+                    builder.MigrationsAssembly("WebApplicationEntity")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
