a053658d3d4ee0c6950453c27bf1bdd7db1924d0
Lucas Quintão <qlucas.dev@gmail.com>
2022-05-01 22:43:54 -0300

    Other Entities and second migration

--- WebApplicationEntity/Data/WebApplicationEntityContext.cs
+++ WebApplicationEntity/Data/WebApplicationEntityContext.cs
@@ -14,6 +14,8 @@ namespace WebApplicationEntity.Data
         {
         }
 
-        public DbSet<WebApplicationEntity.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- WebApplicationEntity/Migrations/20220502013319_OtherEntities.Designer.cs
+++ WebApplicationEntity/Migrations/20220502013319_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplicationEntity.Data;
 
 namespace WebApplicationEntity.Migrations
 {
     [DbContext(typeof(WebApplicationEntityContext))]
-    partial class WebApplicationEntityContextModelSnapshot : ModelSnapshot
+    [Migration("20220502013319_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
